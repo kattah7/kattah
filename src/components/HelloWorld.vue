f7b4314d99d157953235665583dfe6b2ff5bac40
kattah <kattahpro@gmail.com>
2022-07-29 06:20:34 +0900

    Inital Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <h1>This is an test page</h1>
+    <div class="info">
+      <a href="https://github.com/kattah7" target="_blank">Github</a>
+      <br>
+      <a href="https://twitch.tv/kattah" target="_blank">Twitch</a>
+      <br>
+      <a href="https://twitter.com/katt3h" target="_blank">Twitter</a>
+    </div>
+    <h1>Projects</h1>
+    <div class="projetcs">
+      <a href="https://poros.lol" target="_blank">DontAddThisBot</a>
+      <br>
+      <a href="https://twitter.com/OfflineStreamer" target="_blank">OfflineStreamer</a>
+    </div>
+  </div>
+</template>
---
+++ src/views/Error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Error">
+    <img alt="xqcL" src="../assets/xqc.gif">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  created(){
+        document.title = "Error"
+    }
+});
+</script>
